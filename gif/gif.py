902e4cffec10f5f3b0200f13719743a7fcb171d1
Chipmunk <peterrabbit2009@163.com>
2023-11-13 19:39:13 +0800

    just to keep the same

--- gif/gif.py
+++ gif/gif.py
@@ -7,15 +7,15 @@ from PIL import Image
 def compose_gif(pics_list):
     for c in pics_list:
         pics_list[pics_list.index(c)] = Image.open(c)
-    imageio.mimsave("result.gif", pics_list, fps=3)
+    imageio.mimsave("result.gif", pics_list, fps=5)
 
 
 def convert(pics_list):
     for c in pics_list:
         img = Image.open(c)
         img = img.resize((1800, 500))
-        img.save("new"+c)
+        img.save(""+c)
 
 
-compose_gif(["new1.png", "new2.png", "new3.png", "new4.png", "new5.png", "new6.png", "new7.png"])
+compose_gif(["1.png", "2.png", "3.png", "4.png", "5.png", "6.png", "7.png"])
 # convert(["1.png", "2.png", "3.png", "4.png", "5.png", "6.png", "7.png"])
