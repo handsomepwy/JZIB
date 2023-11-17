59784b71e8b6b450f55a9b5d4c6af531e17ef9b6
handsomepwy <peterrabbit2009@163.com>
2023-11-17 21:26:31 +0800

    miss a "=" on line 11, oops

--- mc-alg/buffon.py
+++ mc-alg/buffon.py
@@ -8,7 +8,7 @@ loop_times = 10000000
 for i in tqdm(range(loop_times)):
     x = random.random() / 2
     theta = random.uniform(0, math.pi)
-    if x < math.sin(theta)/2:
+    if x <= math.sin(theta)/2:
         in_point += 1
 print("in point: "+str(in_point))
 result_pi = loop_times / in_point * 2
